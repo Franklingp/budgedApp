df3f3a5f257bcd129b11bfdc62224d0d4650bde2
Franklin Pimentel <franklin.pimentel.1998@gmail.com>
2023-06-14 14:56:55 -0500

    updating the components import and exports

--- App.jsx
+++ App.jsx
@@ -1,32 +1,26 @@
-import { StatusBar } from "expo-status-bar";
-import { StyleSheet, Text, View } from "react-native";
 import { NavigationContainer } from "@react-navigation/native";
 import { createNativeStackNavigator } from "@react-navigation/native-stack";
 
 //Views components
-import Home from "./src/Home";
-import About from "./src/About";
-import Settings from "./src/Settings";
+import { Home, About, Settings } from "./src/index";
 
 export default function App() {
 	const Stack = createNativeStackNavigator();
 
+	//List of routes available in the app
+	const routes = [
+		{ component: Home, name: "Home" },
+		{ component: Settings, name: "Settings" },
+		{ component: About, name: "About" },
+	];
+
 	return (
 		<NavigationContainer>
 			<Stack.Navigator>
-				<Stack.Screen name="Home" component={Home} />
-				<Stack.Screen name="Settings" component={Settings} />
-				<Stack.Screen name="About" component={About} />
+				{routes.map(({ name, component }) => (
+					<Stack.Screen key={"key" + name} name={name} component={component} />
+				))}
 			</Stack.Navigator>
 		</NavigationContainer>
 	);
 }
-
-const styles = StyleSheet.create({
-	container: {
-		flex: 1,
-		backgroundColor: "#fff",
-		alignItems: "center",
-		justifyContent: "center",
-	},
-});
---
+++ src/index.js
@@ -0,0 +1,5 @@
+// file to export and import quicly and easier all the components
+
+export { default as Home } from "./Home";
+export { default as About } from "./About";
+export { default as Settings } from "./Settings";
