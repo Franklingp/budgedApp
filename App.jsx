b156bdf8ed1bc21c0b84b0151ea38a0e58dc99f9
Franklin Pimentel <franklin.pimentel.1998@gmail.com>
2023-06-14 14:23:40 -0500

    Added basic router enviroment

--- App.jsx
+++ App.jsx
@@ -1,12 +1,24 @@
 import { StatusBar } from "expo-status-bar";
 import { StyleSheet, Text, View } from "react-native";
+import { NavigationContainer } from "@react-navigation/native";
+import { createNativeStackNavigator } from "@react-navigation/native-stack";
+
+//Views components
+import Home from "./src/Home";
+import About from "./src/About";
+import Settings from "./src/Settings";
 
 export default function App() {
+	const Stack = createNativeStackNavigator();
+
 	return (
-		<View style={styles.container}>
-			<Text>Hello world!</Text>
-			<StatusBar style="ligth" />
-		</View>
+		<NavigationContainer>
+			<Stack.Navigator>
+				<Stack.Screen name="Home" component={Home} />
+				<Stack.Screen name="Settings" component={Settings} />
+				<Stack.Screen name="About" component={About} />
+			</Stack.Navigator>
+		</NavigationContainer>
 	);
 }
 
